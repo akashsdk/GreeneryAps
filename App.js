79527260494159e779b0a239f964d91a97e32ccd
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-04-15 23:47:50 +0600

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>akash ok ok</Text>
+      <Text>akash </Text>
       <StatusBar style="auto" />
     </View>
   );
